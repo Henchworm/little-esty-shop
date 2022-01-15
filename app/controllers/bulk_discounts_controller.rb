2c97a8efeb61af0055301af99fd5b572a4e99f3a
Chris Hewitt <agop5134@gmail.com>
2022-01-15 15:18:45 -0700

    Complete user story 6, update, with happy and sad path tests

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -6,7 +6,7 @@ class BulkDiscountsController < ApplicationController
   end
 
   def show
-    @bulk_discount = BulkDiscount.find_by(bulk_discount_params)
+    @bulk_discount = BulkDiscount.find(params[:id])
   end
 
   def new
@@ -29,6 +29,24 @@ class BulkDiscountsController < ApplicationController
     BulkDiscount.find_by(bulk_discount_params).delete
 
     redirect_to merchant_bulk_discounts_path(params[:merchant_id]),  notice: "Bulk discount destroyed."
+  end
+
+  def edit
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  def update
+    bulk_discount = BulkDiscount.find(params[:id])
+    bulk_discount.assign_attributes(bulk_discount_params)
+    if bulk_discount.valid?
+      bulk_discount.update(bulk_discount_params)
+      redirect_to merchant_bulk_discount_path(params[:merchant_id], params[:id])
+    else
+      respond_to do |format|
+        format.html {redirect_to request.referrer}
+      end
+      flash[:alert] = bulk_discount.errors.full_messages.join("") + "!"
+    end
   end
 
   private
